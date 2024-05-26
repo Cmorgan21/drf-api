cb48ac3accdf57b643c61321720634eaebc9adad
Callum <callummorgan666@gmail.com>
2024-05-26 23:08:11 +0100

    Fix incorrect middleware configuration

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -124,8 +124,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
 ]
 
 ROOT_URLCONF = 'drf_api.urls'
