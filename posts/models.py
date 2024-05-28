d0dd40799f12d3c54b585c2f5fa6332c87d569be
Callum <callummorgan666@gmail.com>
2024-05-28 12:45:00 +0100

    remove . notation for images

--- posts/models.py
+++ posts/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_bakzsg', blank=True
+        upload_to='images/', default='default_post_bakzsg', blank=True
     )
     image_filter = models.CharField(
         max_length=32, default='normal'
--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_ibf2xs'
+        upload_to='images/', default='default_profile_ibf2xs'
     )
 
     class Meta:
